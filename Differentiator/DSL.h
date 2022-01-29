adf5ff0844e92e1f7e68594c26cc5e1b634183bb
mikhail <shim.01@mail.ru>
2022-01-29 04:14:07 +0300

    some staff

--- Differentiator/DSL.h
+++ Differentiator/DSL.h
@@ -30,7 +30,7 @@
 #define _2_str(name) #name
 #define create_undefined_node() create_node(UNDEFINED, (value_t)((char*)NULL))
 #define create_const_node(num)  create_node(CONST,     (value_t)((double)num))
-#define create_oper_node(oper)  create_node(OPER,      (value_t)((double)oper))
+#define create_oper_node(oper)  create_node(OPERATOR,  (value_t)((double)oper))
 #define create_func_node(func)  create_node(FUNC,      (value_t)func)
 #define create_empty_node()     create_node(EMPTY,     (value_t)((char*)NULL))
 
